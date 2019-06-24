555d53326d6650136dbfc780dbb96d941a160682
ttang <d_ttang@163.com>
2019-06-24 18:20:32 +0800

    readable file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
+const fs = require('fs')
 var os = require('os');
 var pty = require('node-pty');
  
@@ -40,6 +41,7 @@ app.ws('/shell', function(ws, req){
     // });
     // For all shell data send it to the websocket
     ptyProcess.on('data', (data) => {
+      console.log(data)
         ws.send(data);
     });
     // For all websocket data send it to the shell
@@ -48,5 +50,32 @@ app.ws('/shell', function(ws, req){
     });
 
 })
+
+// const readme = fs.createReadStream('./readme.md')
+
+// app.ws('/shell/readme', function(ws, req){
+//   // var ptyProcess = pty.spawn(shell, [], {
+//   //     name: 'xterm-color',
+//   //     cols: 80,
+//   //     rows: 30,
+//   //     cwd: process.env.HOME,
+//   //     env: process.env
+//   //   });
+//   // const shell = pty.spawn('/bin/bash', [], {
+//   //     name: 'xterm-color',
+//   //     cwd: process.env.PWD,
+//   //     env: process.env
+//   // });
+//   // For all shell data send it to the websocket
+//   readme.on('data', (data) => {
+//       console.log(data)
+//       ws.send(data);
+//   });
+//   // For all websocket data send it to the shell
+//   // ws.on('message', (msg) => {
+//   //   ptyProcess.write(msg);
+//   // });
+
+// })
  
 app.listen(3300);
